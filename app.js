e1613d0210c319d77fd2c31175a4511887b3c428
keksPass <kkhut15@freeuni.edu.ge>
2021-02-22 00:43:37 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '18.222.137.70';
+const port = 8080;
+
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
